2d8a8fb696477b6699f64fcce06be5985484a1c2
Ram Vinoth Ponnarasu <ramvinoth37@gmail.com>
2020-11-10 16:44:50 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// our dependencies
+const express = require('express');
+const app = express();
+
+// from top level path e.g. localhost:3000, this response will be sent
+app.get('/', (request, response) => response.send('Hello World'));
+
+app.post('/gms/rest/thirdparty/identityInfo', (request, response) => response.send('Hello World'));
+
+// set the server to listen on port 3000
+app.listen(3000, () => console.log('Listening on port 3000'));
